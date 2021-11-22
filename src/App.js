8da09be6b026ce8ba5f53092e718b21c7a563a06
Ellie Judge <judge_ellie.com>
2021-11-22 20:51:46 +0000

    Adds code for useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Child1 from './components/Child1'
 import { createContext } from 'react';
+import Posts from './components/Posts'
 
 export const Context = createContext('Default Value');
 
@@ -12,10 +13,16 @@ function App() {
     <div className="App">
       <h1>React Hooks and Making API Calls</h1>
 
+
+      {/* To demonstrate useEffect */}
+      <Posts />
+
+
+      {/* To demonstrate useContext */}
       <Context.Provider value={value}>
         <Child1 />
       </Context.Provider>
-      
+
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function Posts() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+    const getPosts = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => { setPosts(response.data) })
+            .catch((error) => { console.log(error) })
+    }
+
+    return (
+        <>
+            {posts.map((post) => <h2>{post.title}</h2>)}
+        </>
+    )
+}
+
+
+
+export default Posts;
